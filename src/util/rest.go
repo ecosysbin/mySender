6f0fa31f2570e7766d0852e5d8654d1f26aa6af9
FireRefrigerator <33680609+FireRefrigerator@users.noreply.github.com>
2019-08-21 15:16:13 +0800

    Update rest.go

--- src/util/rest.go
+++ src/util/rest.go
@@ -1,21 +1,42 @@
 package util
 
 import (
-	"fmt"
 	"net/http"
+	"crypto/tls"
+	"io"
+	"fmt"
 )
 
-var client *http.Client
-
-func NewClient() (error, *http.Client) {
-	fmt.Println("get client")
-	return nil, http.DefaultClient
+type Client struct {
+	Client *http.Client
 }
 
-func NewRequest() {
-
+func NewClient() *Client {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+	client := Client{&http.Client{Transport:tr}}
+	return &client
 }
 
-func SendRequest() {
+func NewRequest(method string, url string, body io.Reader, headers map[string]string) (*http.Request, error) {
+	request, err := http.NewRequest(method, url, body)
+	if err != nil {
+		return  nil, err
+	}
+	if headers != nil {
+		fmt.Println("header is not nil")
+		for headerName, value := range headers {
+			request.Header.Set(headerName,value)
+		}
+	}
+	return request, err
+}
 
+func (c *Client) SendRequest(r *http.Request) (*http.Response, error) {
+	// res := http.Response{}
+	res, err := c.Client.Do(r)
+	return res, err
 }
